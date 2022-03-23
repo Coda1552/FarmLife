ba317dddb4615529d158cb124316c5916b87a1a8
Coda <cody.koelliker@outlook.com>
2022-03-23 00:12:59 -0600

    update textures, made tri bulls aggressive when milked

--- src/main/java/teamdraco/farmlife/common/entities/DomesticTribullEntity.java
+++ src/main/java/teamdraco/farmlife/common/entities/DomesticTribullEntity.java
@@ -62,6 +62,8 @@ public class DomesticTribullEntity extends Animal implements IAnimatable, IAnima
             p_28298_.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
             ItemStack itemstack1 = ItemUtils.createFilledResult(itemstack, p_28298_, FLItems.TRIBULL_MILK.get().getDefaultInstance());
             p_28298_.setItemInHand(p_28299_, itemstack1);
+
+            setTarget(p_28298_);
             return InteractionResult.sidedSuccess(this.level.isClientSide);
         } else {
             return super.mobInteract(p_28298_, p_28299_);
